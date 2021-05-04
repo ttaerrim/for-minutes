8ff15a2688d1115d3fb56d1c69b9fd31bbc6d062
Leehayeon <ll51994060@gmail.com>
2021-05-04 18:59:06 +0900

    mysql

---
+++ forminutesprj/my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',    
+        'NAME': 'meeting',                  
+        'USER': 'root',                     
+        'PASSWORD': '4060',                  
+        'HOST': 'localhost',                     
+        'PORT': '3306',                          
+    }
+}
+
+SECRET_KEY = 'django-insecure-#3m2^xo8yo3w8jzg_-%pew-u5(m!2c##av(@sdl1c=6(m-)6p&'
