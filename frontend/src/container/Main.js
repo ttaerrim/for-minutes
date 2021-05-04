712c5e36611b41dcee955c92dd67c1571ddda2d9
ttaerrim <ltr0121@naver.com>
2021-05-04 20:50:10 +0900

    Header, Footer 초안

---
+++ frontend/src/container/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import Header from "../component/Header";
+import Footer from "../component/Footer";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        Main
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Main;
