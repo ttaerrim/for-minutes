782b2d8fad010a811c2109e91c30fe97fe1905d0
Leehayeon <ll51994060@gmail.com>
2021-05-06 21:05:31 +0900

    modify meeting

--- testapp/urls.py
+++ testapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .api import MeetingApi, MeetingCreateApi, MeetingUpdateApi, MeetingDeleteApi
 
 urlpatterns = [
-    path('api', MeetingApi.as_view()),
-    path('api/create', MeetingCreateApi.as_view()),
-    path('api/<int:pk>', MeetingUpdateApi.as_view()),
-    path('api/<int:pk>/delete', MeetingDeleteApi.as_view()),
+    path('api/', MeetingApi.as_view()),
+    path('api/create/', MeetingCreateApi.as_view()),
+    path('api/<int:pk>/', MeetingUpdateApi.as_view()),
+    path('api/<int:pk>/delete/', MeetingDeleteApi.as_view()),
 ]
