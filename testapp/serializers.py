f688a45e9507d992ea8eed2086e01df0298d616a
Leehayeon <ll51994060@gmail.com>
2021-05-06 13:42:54 +0900

    default

--- testapp/serializers.py
+++ testapp/serializers.py
@@ -1,7 +1,2 @@
 from rest_framework import  serializers
 from .models import Hi
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Hi
-        fields = ('full', 'summary', 'keyword')
--- testapp/views.py
+++ testapp/views.py
@@ -1,8 +1,2 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import PostSerializer
-from .models import Hi
-
-class PostViewset(viewsets.ModelViewSet):
-    queryset = Hi.objects.all()
-    serializer_class = PostSerializer
