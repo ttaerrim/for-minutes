5d1fc45b250dd05c9454ba8925de59b99168e9d1
KangSuzy <astnwl321@gmail.com>
2021-05-13 20:44:22 +0900

    front - backend 연동 ( 글 목록까지만 ... )

--- testapp/urls.py
+++ testapp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('api/', MeetingApi.as_view()),
     path('api/create/', MeetingCreateApi.as_view()),
     path('api/<int:pk>/', MeetingUpdateApi.as_view()),
-    path('api/<int:pk>/result', ResultUpdateApi.as_view()),
+    path('api/<int:pk>/result/', ResultUpdateApi.as_view()),
     path('api/<int:pk>/delete/', MeetingDeleteApi.as_view()),
 ]
