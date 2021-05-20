77ad5e7670f009222ebae7f4140994cb1d0bc7e6
Leehayeon <ll51994060@gmail.com>
2021-05-20 12:21:52 +0900

    url 충돌해결

--- forminutesprj/urls.py
+++ forminutesprj/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('testapp/', include('testapp.urls')),
