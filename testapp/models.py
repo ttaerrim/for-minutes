4e3c29f583bed38550c5e1569c483ca7e473802c
KangSuzy <astnwl321@gmail.com>
2021-05-25 15:35:30 +0900

    add __str__ function at result model

--- testapp/models.py
+++ testapp/models.py
@@ -22,8 +22,7 @@ class Meeting(models.Model):
     # )
 
     def __str__(self):
-        return self.title #자기 자신을 title로 표현
-
+        return self.title
 
 
 class Result(models.Model):
@@ -35,6 +34,5 @@ class Result(models.Model):
     script = models.TextField()
     keyword = models.TextField()
     summary = models.TextField()
-    # def __str__(self):
-    #     return self.title #자기 자신을 title로 표현
-
+    def __str__(self):
+        return self.meeting.title
