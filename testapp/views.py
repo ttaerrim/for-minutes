eb4b29110c85f8ace5602afe17a113c57c042930
ttaerrim <ltr0121@naver.com>
2021-06-14 18:44:48 +0900

    stt 임시 저장

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+import json
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Meeting, Result
+from testapp.stt import *
 # Create your views here.
+
+def resultCreate(request):
+    meeting = get_object_or_404(Meeting, pk=request.POST['pk'])
+    result = Result()
+    audio = "../media/"+str(meeting.file)
+    print(audio)
+    if __name__ == '__main__':
+        print('main??')
+        res = ClovaSpeechClient().req_upload(file=audio, completion='sync')
+        print("res:", res)
+        data = res.text
+        print("data: ", data)
+        result.script = data["text"]
+        print("result.script: ", result.script)
+        result.save()
+        print("result: ", result)
+    
+    return redirect('/result/' + str(meeting.id))
