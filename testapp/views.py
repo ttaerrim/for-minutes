5a31a7065bc24c51e59d9fe8e05b15170a2429a8
ttaerrim <ltr0121@naver.com>
2021-06-14 21:41:26 +0900

    stt api 사용해서 result 모델 script 필드 추가

--- testapp/views.py
+++ testapp/views.py
@@ -1,17 +1,18 @@
 import json
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Meeting, Result
-from testapp.stt import *
+from .stt import *
 # Create your views here.
 
 def resultCreate(request):
-    meeting = get_object_or_404(Meeting, pk=request.POST['pk'])
+    meeting = get_object_or_404(Meeting, pk=request.POST.get('pk', 1))
     result = Result()
-    audio = "../media/"+str(meeting.file)
-    if __name__ == '__main__':
-        res = ClovaSpeechClient().req_upload(file=audio, completion='sync')
-        data = res.text
-        result.script = data["text"]
-        result.save()
+    audio = "media/"+str(meeting.file)
     
+    res = ClovaSpeechClient().req_upload(file=audio, completion='sync')
+    data = json.loads(res.text)
+    result.script = data['text']
+    result.meeting = meeting
+    result.save()
+
     return redirect('/result/' + str(meeting.id))
