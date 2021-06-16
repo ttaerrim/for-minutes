73983bbe341329e201917b569baf5e6e22b0c4db
KangSuzy <astnwl321@gmail.com>
2021-06-16 14:22:20 +0900

    django framework viewset 수정 !

--- testapp/urls.py
+++ testapp/urls.py
@@ -2,13 +2,27 @@ from django.urls import path
 from .api import *
 import testapp.views
 
-urlpatterns = [
-    path('api', MeetingApi.as_view()),
-    path('api/create', MeetingCreateApi.as_view()),
-    path('api/<int:pk>', MeetingUpdateApi.as_view()),
-    path('api/<int:pk>/delete', MeetingDeleteApi.as_view()),
+from testapp.views import MeetingViewSet
 
-    path('api/result', ResultApi.as_view()),
-    path('api/result/create', testapp.views.resultCreate, name="result_create"),
-    path('api/result/<int:pk>/', ResultUpdateApi.as_view()),
+from django.conf.urls import url,include 
+from django.contrib import admin 
+from rest_framework import routers
+
+
+router = routers.DefaultRouter() 
+router.register('',MeetingViewSet)
+
+
+urlpatterns = [
+    # path('api', MeetingApi.as_view()),
+    # path('api/create', MeetingCreateApi.as_view()),
+    # path('api/<int:pk>', MeetingUpdateApi.as_view()),
+    # path('api/<int:pk>/delete', MeetingDeleteApi.as_view()),
+    
+    url(r'^',include(router.urls)),
+
+
+    path('result', ResultApi.as_view()),
+    path('result/create', testapp.views.resultCreate, name="result_create"),
+    path('result/<int:pk>/', ResultUpdateApi.as_view()),
 ]
