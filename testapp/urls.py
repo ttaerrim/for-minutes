80ae1b7f87b527a547caa53dfdc59d24c3292bdc
Leehayeon <ll51994060@gmail.com>
2021-06-16 14:35:26 +0900

    ?

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .api import *
+from .views import *
 
 urlpatterns = [
     path('api/', MeetingApi.as_view()),
     path('api/create/', MeetingCreateApi.as_view()),
-    path('api/<int:pk>/', MeetingUpdateApi.as_view()),
+  #  path('api/<int:pk>/', MeetingUpdateApi.as_view()),
+    path('api/<int:pk>/', updateViewSet.as_view()),
     path('api/<int:pk>/result/', ResultUpdateApi.as_view()),
     path('api/<int:pk>/delete/', MeetingDeleteApi.as_view()),
 ]
