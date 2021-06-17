93690b057f613739dbf24c38653241ffb83ad511
Leehayeon <ll51994060@gmail.com>
2021-06-17 21:42:21 +0900

    upstream pull 취소

--- forminutesprj/settings.py
+++ forminutesprj/settings.py
@@ -148,7 +148,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'frontend', 'build', 'static'),        
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),        
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
