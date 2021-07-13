6f634a544fa1c5edbdc09468eb3093ffe69cbb44
KangSuzy <astnwl321@gmail.com>
2021-07-13 20:50:22 +0900

    Backend (#46)
    * django-mysql 연동
    
    * fixed untracked files
    
    * front - backend 연동 ( 글 목록까지만 ... )
    
    * front - back merge 1
    
    * front - back merge 1
    
    * updated master merge
    
    * add __str__ function at result model
    
    * modified urls
    
    * backend stt ex
    
    * 병합 전 저장
    
    * pull upstream for stt ex
    
    * import error modified
    
    * 프론트 수정중
    
    * django framework viewset 수정 !
    
    * Header 수정
    
    * merge modified viewset
    
    * F12 눌렀을 때 나오는 manifest.json 오류 수정
    
    * Result 결과 생성 시 새로고침
    
    * renderResult 기능 Result.js 파일로 분리
    
    * 글 최신순으로 불러오기
    
    * Post.js 버튼 색깔 변경
    
    * 글 삭제 기능 구현
    
    * Detail 페이지 파일 이름 삭제
    
    * 0621 가전시용 ..
    
    * 0621 가전시 화이팅 ..
    
    * .gitignore 수정
    
    * .gitignore 수정
    
    * .gitignore 수정
    
    * gitignore 수정git add .!
    
    * 프론트중간
    
    * 0621 merge front
    
    * result model keyword/summary null 값 추가
    
    * merge front
    
    * viewset router -> as_View() 수정
    
    * modified urls.py - for result to as_view()
    
    * modified models and result.js
    
    * modified models and views
    
    * ResultViewSet create 함수 수정, url 과 detail,result 수정
    
    Co-authored-by: JinJooYoo <43843247+JinJooYoo@users.noreply.github.com>
    Co-authored-by: ttaerrim <ltr0121@naver.com>
    Co-authored-by: ttaerrim <ttrrr121@gmail.com>
    Co-authored-by: LeeHaYeon <43868551+hayeon4060@users.noreply.github.com>
    Co-authored-by: Taerim Lee <43867711+ttaerrim@users.noreply.github.com>

--- testapp/views.py
+++ testapp/views.py
@@ -6,21 +6,20 @@ import os
 from forminutesprj.settings import MEDIA_ROOT
 from testapp.api import *
 
-# Create your views here.
 from .serializer import MeetingSerializer,ResultSerializer
 from rest_framework import viewsets
+
 # list detail update delate
 class MeetingViewSet(viewsets.ModelViewSet):
     queryset = Meeting.objects.all()
     serializer_class = MeetingSerializer
 
-
 class ResultViewSet(viewsets.ModelViewSet):
     queryset = Result.objects.all()
     serializer_class = ResultSerializer
 
-    def create(self,request):
-        meeting = get_object_or_404(Meeting, pk=request.POST.get('pk', 1))
+    def create(self,request,pk):
+        meeting = get_object_or_404(Meeting, pk=pk)
         result = Result()
         audio = "media/"+str(meeting.file)
         
@@ -29,4 +28,4 @@ class ResultViewSet(viewsets.ModelViewSet):
         result.script = data['text']
         result.meeting = meeting
         result.save()
-        return redirect('/result/' + str(meeting.id))
+        return redirect('/testapp/result/' + str(meeting.id))
