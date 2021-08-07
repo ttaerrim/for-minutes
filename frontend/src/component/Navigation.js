7e0559c316349948118364e6211862da27be0c40
ttaerrim <ltr0121@naver.com>
2021-08-07 12:15:06 +0900

    Navigation 수정

--- frontend/src/component/Navigation.js
+++ frontend/src/component/Navigation.js
@@ -1,8 +1,8 @@
 import React from "react";
 //import { Link } from "react-router-dom";
-import { Heading, Text,Link,Table,Box} from "gestalt";
+import { Heading, Text, Link, Table, Box } from "gestalt";
 import "gestalt/dist/gestalt.css";
-import './Navigation.css';
+import "./Navigation.css";
 import logo from "../images/fmlogo.png";
 
 import {
@@ -11,50 +11,62 @@ import {
   Route,
   useParams,
 } from "react-router-dom";
-import { Navbar,Nav,NavDropdown,Form,FormControl,Button ,Dropdown,DropdownButton} from 'react-bootstrap'
-
-
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+  Dropdown,
+  DropdownButton,
+} from "react-bootstrap";
 
 const Navigation = () => {
   return (
     <div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <Router>
-                            <Navbar bg="white" variant="light" expand="lg" sticky="top" >
-                                <Navbar.Brand href="/">
-                                <img
-                                    alt=""
-                                    src={logo}
-                                    width="30"
-                                    height="30"
-                                    className="d-inline-block align-top"
-                                    />{' '}
-                                    </Navbar.Brand>
-                                    <Navbar.Text>For minutes</Navbar.Text>
-                                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end navalign" >
-                                <Navbar.Text>
-                                    <Nav className="mr-auto">
-                                    <Nav.Link href="/">Home</Nav.Link>
-                                    <Nav.Link href="/about">About Us</Nav.Link>
-                                    <NavDropdown align="end" title="My" id="collasible-nav-dropdown">
-                                        <NavDropdown.Item href="/minutes">MinutesList</NavDropdown.Item>
-                                        <NavDropdown.Item href="/post">Post</NavDropdown.Item>
-                                        
-                                    </NavDropdown>
-                                    
-                                    </Nav>
-                                    </Navbar.Text>
-                                </Navbar.Collapse>
-                            </Navbar>
-                            <br />
-                        </Router>
-                    </div>
-                </div>
-            </div>
-    
-    
+      <div className="row">
+        <div className="col-md-12">
+          <Router>
+            <Navbar bg="white" variant="light" expand="lg" sticky="top">
+              <Navbar.Brand href="/">
+                <img
+                  alt=""
+                  src={logo}
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-top"
+                />{" "}
+              </Navbar.Brand>
+              <Navbar.Text>For minutes</Navbar.Text>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse
+                id="basic-navbar-nav"
+                className="justify-content-end navalign"
+              >
+                <Navbar.Text>
+                  <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/about">About Us</Nav.Link>
+                    <NavDropdown
+                      align="end"
+                      title="My"
+                      id="collasible-nav-dropdown"
+                    >
+                      <NavDropdown.Item href="/minutes">
+                        MinutesList
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="/post">Post</NavDropdown.Item>
+                    </NavDropdown>
+                  </Nav>
+                </Navbar.Text>
+              </Navbar.Collapse>
+            </Navbar>
+            <br />
+          </Router>
+        </div>
+      </div>
+    </div>
   );
 };
 
